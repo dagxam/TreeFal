3330a3a94cdfe0205ad59c9406115e908799e0dd
dagxam <dagxam@gmail.com>
2026-08-19 14:23:35 +0300

    Track incomplete tree scans explicitly

--- src/main/java/me/dagxam/treefall/TreeBlocks.java
+++ src/main/java/me/dagxam/treefall/TreeBlocks.java
@@ -1,6 +1,12 @@
 package me.dagxam.treefall;
 
 import org.bukkit.block.Block;
+
 import java.util.Set;
 
-public record TreeBlocks(Set<Block> logs, Set<Block> leaves) {}
+public record TreeBlocks(Set<Block> logs, Set<Block> leaves, boolean truncated) {
+
+    public TreeBlocks(Set<Block> logs, Set<Block> leaves) {
+        this(logs, leaves, false);
+    }
+}
